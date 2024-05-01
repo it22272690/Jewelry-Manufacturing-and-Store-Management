84c45a398b0b6243eb114f6546ea4e5232826f59
Thimalka Indusara <thimalka.indusara@gmail.com>
2024-05-01 15:40:34 +0530

    new commits

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,45 +1,43 @@
-//password SksYpipUUkLVaaZ4
+// Load environment variables from a .env file into process.env
+require("dotenv").config();
 
-require('dotenv').config();
-
+// Import required modules
 const express = require("express");
 const mongoose = require("mongoose");
-const routerM = require("./Routes/MaterialRoutes"); //single dot for the path
-
-//new
-const routerP = require("./Routes/ProductRoutes"); //single dot for the path
+const routerM = require("./Routes/MaterialRoutes");
+const routerP = require("./Routes/ProductRoutes");
 const routerS = require("./Routes/SupplierRoutes");
 const routerR = require("./Routes/ReserveRoutes");
 const routerMO = require("./Routes/MaterialoutRoutes");
 const routerMI = require("./Routes/MaterialinRoutes");
 
+// Create an instance of the express application
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-//Middleware
-/*app.use("/",(req,res,next)=>{
-    res.send("Thimalka, It's working!");
-})*/
-//above code worked!
-
+// Middleware to parse JSON bodies and enable CORS
 app.use(express.json());
 app.use(cors());
-app.use("/materials",routerM);
-//above is the one which is checked by postman; it worked!
 
-//new
-app.use("/products",routerP);
-app.use("/suppliers",routerS);
-app.use("/reserves",routerR);
-app.use("/materialouts",routerMO);
-app.use("/materialins",routerMI);
+// Define routes for different resources
+app.use("/materials", routerM);
+app.use("/products", routerP);
+app.use("/suppliers", routerS);
+app.use("/reserves", routerR);
+app.use("/materialouts", routerMO);
+app.use("/materialins", routerMI);
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
+// Connect to MongoDB database using environment variables
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
   .then(() => {
+    // Start the server and listen on the specified port
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.log(err));
-
